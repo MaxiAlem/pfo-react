02a90f5499298657185cac5cd182867bceeac2d8
MaxiAlem <maerc.maxi@gmail.com>
2025-05-21 22:10:59 -0300

    first commit ameo

---
+++ src/routes/index.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+
+
+const AppRoutes = () => {
+  return (<Routes>
+    
+    <Route element={<Home/>}>
+   
+
+    </Route>
+  </Routes>
+  )
+};
+export default AppRoutes
