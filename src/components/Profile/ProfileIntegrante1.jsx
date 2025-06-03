3736e9731a713aa307930ae74f17b8991445b149
MaxiAlem <maerc.maxi@gmail.com>
2025-06-03 01:06:03 -0300

    cambios de prueba

--- src/components/Profile/ProfileIntegrante1.jsx
+++ src/components/Profile/ProfileIntegrante1.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { ThemeContext } from '../../context/ThemeContext';
+import { SiHtml5, SiCss3, SiJavascript, SiGit } from 'react-icons/si';
+import { FaReact } from 'react-icons/fa';
 import './ProfileIntegrante1.css';
 
+//profile
+import terricola from '../../assets/ProfileIntegrante1/terricola.webp'
+//pelis
+import the_thing from '../../assets/ProfileIntegrante1/the_thing.jpg'
+import the_warriors from '../../assets/ProfileIntegrante1/Thewarriors.webp'
+import marsAttack from '../../assets/ProfileIntegrante1/marsAttack.jpg';
+
+
+
+
+
+
 const ProfileIntegrante1 = () => {
+
+  const { isDarkMode } = useContext(ThemeContext);
   return (
-    <div className="profile-page">
-      <h1>Perfil de Integrante 1</h1>
-      <div className="profile-content">
-        {/* <img
-          src='/../assets/ProfileIntegrante1/profile.jpg'
-          alt="Integrante 1"
-          className="profile-photo"
-        /> */}
-        <div className="profile-details">
-          <h2>Juan Pérez</h2>
-          <p><strong>Rol:</strong> Desarrollador Frontend (Estructura y Diseño)</p>
-          <p><strong>Descripción:</strong> Soy Juan, estudiante de Técnico Superior en Desarrollo de Software. En este proyecto, diseñé la página de inicio y configuré la navegación principal, incluyendo el sidebar.</p>
-          <p><strong>Contribuciones:</strong></p>
-          <ul>
-            <li>Desarrollo de la página de inicio con diseño responsivo.</li>
-            <li>Implementación del sidebar con navegación dinámica.</li>
-            <li>Integración de componentes de clima en la página principal.</li>
-          </ul>
+    <>
+      <section className={`profile-card ${isDarkMode ? 'dark' : ''}`}>
+      <div className="card-container">
+        <div className="card-image">
+          <img src={terricola} alt="Foto de perfil" />
+        </div>
+        <div className="card-text">
+          <h2>Hola</h2>
+          <p>De día soy desarrollador web y de noche una sailor scout que lucha por el amor y la justicia.</p>
         </div>
       </div>
-    </div>
+    </section>
+    
+    </>
   );
 };
 
